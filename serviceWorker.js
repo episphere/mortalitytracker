66634fd7df36081c4f7998671cbbe93069410d50
bhaumik55231 <bhaumik55231@gmail.com>
2020-05-10 22:05:08 -0400

    reverting cache storage of API responses.

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,36 +1,14 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst, StaleWhileRevalidate } = workbox.strategies;
-const { CacheableResponsePlugin } = workbox.cacheableResponse;
+const { NetworkFirst } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const googleAnalytics = workbox.googleAnalytics;
 
 googleAnalytics.initialize();
+
 registerRoute(/\.(?:js|css|json)$/, new NetworkFirst({cacheName: 'static-cache'}));
 
 registerRoute(/\.(?:png|jpg|jpeg|svg|gif|ico)$/,
-    new CacheFirst({
-        cacheName: 'images-cache',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 30,
-                maxAgeSeconds: 7 * 24 * 60 * 60,
-            })
-        ]
-    })
+    new NetworkFirst({cacheName: 'images-cache'})
 );
-
-registerRoute(
-    new RegExp('https://data.cdc.gov/.+'),
-    new NetworkFirst({
-        cacheName: 'api-cache',
-        plugins: [
-            new CacheableResponsePlugin({
-                statuses: [200],
-            })
-        ]
-    })
-);
-
-workbox.precaching.precacheAndRoute([{url: 'index.html', revision: `${new Date().getTime()}`}]);
