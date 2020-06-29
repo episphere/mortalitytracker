deb082cdd063c2e2ff9ccc9f6e2166f69170e0a1
Jonas Almeida <jalmeida@mathbiol.org>
2020-06-28 21:03:39 -0400

    json array

--- manuscript1/man1.js
+++ manuscript1/man1.js
@@ -1,8 +1,21 @@
 console.log('man1.js loaded')
 
 
-man1={}
+man1={data:{}}
+
+man1.getExcessAllCause= async ()=>{
+    let data = await (await fetch('https://episphere.github.io/mortalitytracker/manuscript1/excessTotalMortality.json')).json()
+    man1.data.excessAllCause=data
+    return data
+};
+
+// ini
+man1.ini=async _=>{
+    await man1.getExcessAllCause();
+    return 'man1.js initialized'
+}
 
 if(typeof(define)!=='undefined'){
     define(man1)
 }
+
