39b8e9183a85bc78bbc28d5e057db7dac563748d
Jonas Almeida <jalmeida@mathbiol.org>
2020-06-30 23:59:59 -0400

    getcsv method

--- manuscript1/man1.js
+++ manuscript1/man1.js
@@ -15,6 +15,28 @@ man1.ini=async _=>{
     return 'man1.js initialized'
 }
 
+man1.getcsv=async(url='https://episphere.github.io/mortalitytracker/manuscript1/All%20States%20-%20All%20Cause.csv')=>{
+    let arr=[]
+    let txt = await(await fetch(url)).text()
+    let rows=txt.split(/[\n\r]+/).map(r=>r.split(','))
+    let parms= rows[0]
+    rows.slice(1).forEach((r,i)=>{
+        arr[i]=[]
+        r.forEach((v,j)=>{
+            if(parms[j]=='Date'){
+                arr[i][parms[j]]=new Date(v)
+            }else{
+                arr[i][parms[j]]=parseFloat(v)
+            }
+        })
+    })
+    // restrict range to March 3 to May 26
+    let t0 = new Date('2020-03-03T05:00:00.000Z')
+    let tf = new Date('2020-05-26T04:00:00.000Z')
+    arr=arr.filter(x =>(x.Date >= t0)&(x.Date <=tf ))
+    return arr
+}
+
 if(typeof(define)!=='undefined'){
     define(man1)
 }
