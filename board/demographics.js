10341adb51a94dbe433e187d059f7717a40f1edf
enemeth19 <erikanemeth99@gmail.com>
2020-07-10 23:44:31 -0400

    Change y-axis to mortality rate
    -Population data obtained from CDC WONDER

--- board/demographics.js
+++ board/demographics.js
@@ -1,10 +1,5 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// (1) GET CDC DATA
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// (2.0) Get CDC data
 
 function getData(url) {
     let response = '';
@@ -18,29 +13,22 @@ function getData(url) {
 }
 let demographicsURL = getData('https://data.cdc.gov/resource/ks3g-spdg.json?$limit=1000000')
 let CDCdata = JSON.parse(demographicsURL)
-// console.log(CDCdata)
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// --------------------------------------------------------------------------------------------------------
 
-// (2) FILTER AND PLOT CDC DATA
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// (2.1) Organize data by race
-
-const raceData = []
-for (let i = 0; i < CDCdata.length; i++) 
-    raceData.push(CDCdata[i].race_and_hispanic_origin)
-function getUniqueValues(value, index, self) {
-    return self.indexOf(value) === index;
-}
-let uniqueRaces = raceData.filter(getUniqueValues);
+// (2.1) Organize data by races needed for plot
 
 let dataByRace = []
-for(let i = 0; i < uniqueRaces.length; i++) 
-    dataByRace.push(CDCdata.filter(data => data.race_and_hispanic_origin == uniqueRaces[i]))
+dataByRace.push(CDCdata.filter(data => data.race_and_hispanic_origin == "Non-Hispanic White")) // 0 
+dataByRace.push(CDCdata.filter(data => data.race_and_hispanic_origin == "Non-Hispanic Black")) // 1
+dataByRace.push(CDCdata.filter(data => data.race_and_hispanic_origin == "Non-Hispanic American Indian or Alaska Native")) // 2
+dataByRace.push(CDCdata.filter(data => data.race_and_hispanic_origin == "Non-Hispanic Asian")) // 3
+dataByRace.push(CDCdata.filter(data => data.race_and_hispanic_origin == "Non-Hispanic Native Hawaiian or Other Pacific Islander")) // 4
+dataByRace.push(CDCdata.filter(data => data.race_and_hispanic_origin == "Hispanic or Latino")) // 5
+
+// --------------------------------------------------------------------------------------------------------
+
+// (2.2) Get date that data were last updated
 
 const endWeeks = []
 for (let i = 0; i < CDCdata.length; i++) 
@@ -48,30 +36,28 @@ for (let i = 0; i < CDCdata.length; i++)
 
 // --------------------------------------------------------------------------------------------------------
 
-// (2.2) Create traces for plot
+// (2.3) Create array (finalTotalDeaths) with most-updated counts of deaths from COVID-19 for each group
 
-let allTraces = [];
+var allNumDeathsArray = []
 
-function getPlotData(raceArray) {
+function getNumberOfDeaths(raceArray) {
 
-    for(let e = 1; e < raceArray.length; e++) {
-
-        let currentObj = {}
+    for(let e = 0; e < raceArray.length; e++) {
 
         let currentRace = raceArray[e]
 
-        // (2.2.1) Filter properties of all data
+        // (2.3.1) Filter properties of all data
         let filteredProps = []
         for(let i = 0; i < currentRace.length; i++)
             filteredProps.push( (({age_group, covid_19_deaths}) => ({age_group, covid_19_deaths}))(currentRace[i]) );
 
-        //(2.2.2) Remove entries where covid_19_deaths = Undefined
+        //(2.3.2) Remove entries where covid_19_deaths = Undefined
         noUndefineds = []
         for(let i = 0; i < filteredProps.length; i++) 
             if(filteredProps[i]['covid_19_deaths'] != undefined) 
                 noUndefineds.push(filteredProps[i])
 
-        // (2.2.3) Group data by age group
+        // (2.3.3) Group data by age group
         function groupBy(arr, property) {
             return arr.reduce(function(memo, x) {
                 if (!memo[x[property]])
@@ -82,10 +68,10 @@ function getPlotData(raceArray) {
         }
         var groupedData = groupBy(noUndefineds, 'age_group'); 
 
-        // (2.2.4) Get all unique ages
+        // (2.3.4) Get all unique ages
         let uniqueAges = Object.getOwnPropertyNames(groupedData)
 
-        // (2.2.5) Get number of deaths 
+        // (2.3.5) Get number of deaths 
         let numDeathsArray = []
         numDeathsArray.length = uniqueAges.length;
         numDeathsArray.fill(0)
@@ -93,44 +79,109 @@ function getPlotData(raceArray) {
             for(let j = 0; j < noUndefineds.length; j++) 
                 if(noUndefineds[j].age_group == uniqueAges[i]) 
                     numDeathsArray[i] += parseInt(noUndefineds[j].covid_19_deaths);
-
-        // (2.2.6) Append data to current trace (i.e., currentObj)
-        currentObj.x = uniqueAges;
-        currentObj.y = numDeathsArray;
-        currentObj.name = uniqueRaces[e]; // corresponds to text outside tooltip box and legend
-        currentObj.type = 'bar'
-        allTraces.push(currentObj);
+        allNumDeathsArray.push(numDeathsArray)
     }
 
+    // (2.3.6) Merge data for "Non-Hispanic Asian" and "Non-Hispanic Native Hawaiian or Other Pacific Islander"
+    for(let i = 0; i < allNumDeathsArray[3].length; i++) 
+        allNumDeathsArray[3][i] += allNumDeathsArray[4][i]
+    let finalTotalDeaths = [];
+    for(let i = 0; i < allNumDeathsArray.length; i++)
+        if(i != 4)
+            finalTotalDeaths.push(allNumDeathsArray[i])
+
+    return finalTotalDeaths
+
 }
 
-getPlotData(dataByRace);
+let finalTotalDeaths = getNumberOfDeaths(dataByRace)
 
 // --------------------------------------------------------------------------------------------------------
 
-// (2.3) Set up plot layout
+// (2.4) Create array (populationsArr) of populations for each group
 
-let layout = {
-    barmode: 'group',
-    title: 'COVID-19 Mortality in the United States: Deaths to\t' + endWeeks[0].slice(0,10),
-    xaxis: {
-        title: 'Age Group',
-    },
-    yaxis: {
-        title: {
-            text: 'Number of Deaths',
-            standoff: 10
+d3.csv('cdcWONDER_population.csv').then(function(data) {
+
+    let finalUniqueRaces = ["Non-Hispanic White", "Non-Hispanic Black", "Non-Hispanic American Indian or Alaska Native", "Non-Hispanic Asian or Pacific Islander", "Hispanic or Latino"]
+
+    let populationsArr = new Array(5).fill(0).map(() => new Array(12).fill(0));
+
+    // Group data by age group
+    function groupBy(arr, property) {
+        return arr.reduce(function(memo, x) {
+            if (!memo[x[property]])
+                memo[x[property]] = []; 
+            memo[x[property]].push(x);
+            return memo;
+        }, {});
+    }
+    var groupedData = groupBy(noUndefineds, 'age_group'); 
+
+    // Get all unique ages
+    let uniqueAges = Object.getOwnPropertyNames(groupedData)
+    
+    for(let i = 0; i < finalUniqueRaces.length; i++) 
+        for(let j = 0; j < uniqueAges.length; j++) 
+            for(let k = 0; k < data.length; k++) 
+                if(data[k].race_and_hispanic_origin == finalUniqueRaces[i] && data[k].age_group == uniqueAges[j]) 
+                    populationsArr[i][j] = parseInt(data[k].population);
+                
+
+// --------------------------------------------------------------------------------------------------------
+
+    // (2.5) Create array (mortalityRateArray) of mortality rates for each group
+
+    let mortalityRateArray = new Array(5).fill(0).map(() => new Array(12).fill(0));
+
+    for(let i = 0; i < mortalityRateArray.length; i++) 
+        for(let j = 0; j < mortalityRateArray[i].length; j++) 
+            mortalityRateArray[i][j] = (finalTotalDeaths[i][j] / populationsArr[i][j]) * 100000
+
+// --------------------------------------------------------------------------------------------------------
+
+    // (2.6) Create array with traces for plot
+
+    let arrayForPlot = [];
+
+    for(let i = 0; i < 5; i++) {
+        let thisObj = {};
+        thisObj.name = finalUniqueRaces[i];
+        thisObj.type = 'bar';
+        thisObj.x = uniqueAges;
+        thisObj.y = mortalityRateArray[i];
+        arrayForPlot.push(thisObj);
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+
+    // (2.7) Set up plot layout
+
+    let layout = {
+        barmode: 'group',
+        title: 'COVID-19 Mortality Rate (per 100,000 population) in the United States: <span><br></span>Deaths to\t' + endWeeks[0].slice(0,10),
+        xaxis: {
+            title: 'Age Group',
+        },
+        yaxis: {
+            title: {
+                text: 'Mortality Rate (per 100,000)',
+                standoff: 10
+            }
+        },
+        showlegend: true,
+        hoverlabel: {namelength: -1} // ensures that text outside tooltip box is not cut off
+    }
+        
+    // --------------------------------------------------------------------------------------------------------
+
+    // (2.8) Plot
+
+    Plotly.newPlot(
+        'demographicsDiv', 
+        {
+            data: arrayForPlot, 
+            layout: layout
         }
-    },
-    showlegend: true,
-    hoverlabel: {namelength: -1} // ensures that text outside tooltip box is not cut off
-}
+    )
 
-// --------------------------------------------------------------------------------------------------------
-
-// (2.4) Plot data
-
-Plotly.newPlot('demographicsDiv', allTraces, layout);
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+})
