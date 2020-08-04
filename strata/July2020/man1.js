4c1bf23e0e7d7d3babd3a2177bd850bda18796c0
Jonas Almeida <jalmeida@mathbiol.org>
2020-08-03 22:00:17 -0400

    missing csv

---
+++ strata/July2020/man1.js
@@ -0,0 +1,42 @@
+console.log('man1.js loaded')
+
+
+man1={data:{}}
+
+man1.getExcessAllCause= async ()=>{
+    let data = await (await fetch('https://episphere.github.io/mortalitytracker/manuscript1/excessTotalMortality.json')).json()
+    man1.data.excessAllCause=data
+    return data
+};
+
+// ini
+man1.ini=async _=>{
+    await man1.getExcessAllCause();
+    return 'man1.js initialized'
+}
+
+man1.getcsv=async(url='https://episphere.github.io/mortalitytracker/strate/July2020/All%20States%20-%20All%20Cause.csv')=>{
+    let arr=[]
+    let txt = await(await fetch(url)).text()
+    let rows=txt.split(/[\n\r]+/).map(r=>r.split(','))
+    let parms= rows[0]
+    rows.slice(1).forEach((r,i)=>{
+        arr[i]=[]
+        r.forEach((v,j)=>{
+            if(parms[j]=='Date'){
+                arr[i][parms[j]]=new Date(v)
+            }else{
+                arr[i][parms[j]]=parseFloat(v)
+            }
+        })
+    })
+    // restrict range to January - June 2020
+    let t0 = new Date('2020-01-00T05:00:00.000Z')
+    let tf = new Date('2020-07-00T04:00:00.000Z')
+    arr=arr.filter(x =>(x.Date >= t0)&(x.Date <=tf ))
+    return arr
+}
+
+if(typeof(define)!=='undefined'){
+    define(man1)
+}
