9c6cdbb5dab01b2203ec841aa9b60b875de4a2e8
Jonas Almeida <jalmeida@mathbiol.org>
2020-08-06 23:09:55 -0400

    interactive tabulation

--- strata/strata.js
+++ strata/strata.js
@@ -12,7 +12,8 @@ strata.getData=async()=>{
     let xx = await d3.json(url)
     strata.data.stratParms=["sex", "race_ethnicity", "age_group"]
     strata.data.numParms=[ "year", "mmwrweek", "allcause", "naturalcause", "septicemia_a40_a41", "malignant_neoplasms_c00_c97", "diabetes_mellitus_e10_e14", "alzheimer_disease_g30", "influenza_and_pneumonia_j09", "chronic_lower_respiratory", "other_diseases_of_respiratory", "nephritis_nephrotic_syndrome", "symptoms_signs_and_abnormal", "diseases_of_heart_i00_i09", "cerebrovascular_diseases", "covid_19_u071_multiple_cause", "covid_19_u071_underlying"]
-    strata.data.dateParms=["data_as_of", "analysis_period_start_date", "analysis_period_end_date",]
+    strata.data.dateParms=["data_as_of", "analysis_period_start_date", "analysis_period_end_date"]
+    strata.data.causes=["allcause", "naturalcause", "septicemia_a40_a41", "malignant_neoplasms_c00_c97", "diabetes_mellitus_e10_e14", "alzheimer_disease_g30", "influenza_and_pneumonia_j09", "chronic_lower_respiratory", "other_diseases_of_respiratory", "nephritis_nephrotic_syndrome", "symptoms_signs_and_abnormal", "diseases_of_heart_i00_i09", "cerebrovascular_diseases", "covid_19_u071_multiple_cause", "covid_19_u071_underlying"]
 
     xx = xx.map(x=>{
         strata.data.numParms.forEach(k=>{
@@ -43,15 +44,23 @@ strata.getData=async()=>{
 
 strata.selectCounts=(div0=document.getElementById("countTabDiv"))=>{
     //let h='<input id="maleRadio" type="radio" value="Male" checked=true> Male<input id="femaleRadio" type="radio" value="Female" checked=true> Female'
-    let h=''
+    let h='<table><tr><td>'
     Object.keys(strata.data.stratValues).forEach(k=>{
         h+=`<b style="color:maroon">${k}:</b>`
         strata.data.stratValues[k].forEach(v=>{
-            h+=`<br><input id="${v}Radio" type="checkbox" value='{"${k}":"${v}"}' checked=true onchange="strata.filterSelected()"> ${v}`
+            h+=`<br><input id="${v}Radio" class="strata" type="checkbox" value='{"${k}":"${v}"}' checked=true onchange="strata.filterSelected()"> ${v}`
         })
         h+='<br>'
     })
-    h+='<div id=tabCountDiv></div>'
+    h+='</td><td>'
+    h+=`<b style="color:maroon">Causes:</b>`
+    strata.data.causes.forEach(c=>{
+        h+=`<br><input id="${c}Radio" class="cause" type="checkbox" value="${c}" checked=true onchange="strata.filterSelected()"> ${c}`
+    })
+    h += '</td></tr></table>'
+    h+='<hr>'
+    h+='<div id=tabulateDiv></div>'
+    h+='<hr>'
     div0.innerHTML=h
     strata.div0=div0
     return div0
@@ -61,7 +70,7 @@ strata.filterSelected=()=>{
     // filter
     console.log('----begin----')
     strata.filters={}
-    let filters = [...strata.div0.querySelectorAll('input')].filter(ip=>(!ip.checked)).forEach(ip=>{
+    let filters = [...strata.div0.querySelectorAll('input')].filter(ip=>(!ip.checked)&(ip.className=='strata')).forEach(ip=>{
         console.log(ip.value)
         let fi = JSON.parse(ip.value)
         let k=Object.keys(fi)[0]
@@ -83,6 +92,49 @@ strata.filterSelected=()=>{
         return res
     })
     console.log(strata.filters,strata.data.filtered)
+    strata.tabulate()
+}
+
+strata.tabulate=(div=document.getElementById('tabulateDiv'),dt=strata.data.filtered,parms=strata.data.causes)=>{ // tabulate filtered results
+    div.innerHTML=''
+    let tb=document.createElement('table')
+    //let race = [...new Set(strata.data.filtered.map(x=>x.race_ethnicity))]
+    let age = [...new Set(strata.data.filtered.map(x=>x.age_group))]
+    let causes = [...strata.div0.querySelectorAll('input')].filter(ip=>(ip.checked)&(ip.className=='cause')).map(ip=>ip.value)
+    // header
+    let trh = document.createElement('tr')
+    tb.appendChild(trh)
+    let thCause = document.createElement('th')
+    thCause.innerHTML="Age &#8594;<br> <br>&#8595; Cause"
+    thCause.style.color='maroon'
+    thCause.align="left"
+    trh.appendChild(thCause)
+    age.forEach(ag=>{
+        let th = document.createElement('th')
+        th.align="right"
+        th.style.color='black'
+        th.innerHTML=ag
+        trh.appendChild(th)
+    })
+    causes.forEach(c=>{
+        let tr = document.createElement('tr')
+        tb.appendChild(tr)
+        let td = document.createElement('td')
+        td.innerHTML=c
+        td.style.color='black'
+        td.style.fontWeight='bold'
+        tr.appendChild(td)
+        age.forEach(ag=>{
+            let td = document.createElement('td')
+            td.align="right"
+            td.innerHTML=dt.filter(x=>(x.age_group==ag)).map(x=>x[c]).reduce((a,b)=>a+b)
+            tr.appendChild(td)
+        })
+    })
+
+
+
+    div.appendChild(tb)
 }
 
 strata.ini=async()=>{
