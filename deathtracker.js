09fef6aff52f32a69a8c793ab882470380e3fc5f
Jonas Almeida <jalmeida@mathbiol.org>
2021-01-22 10:25:17 -0500

    ref 2016-19

--- deathtracker.js
+++ deathtracker.js
@@ -524,7 +524,9 @@ dtrack.plotlyCompare=async(div='plotlyCompareDiv')=>{
         traces.push(trace)
     }
     dtrack.data.traces=traces
-    dtrack.data.years.sort().slice(0,-1).forEach(yr=>{ // all years exept the last, 2020
+    //dtrack.data.years.sort().slice(0,-2).forEach(yr=>{ // all years exept the last two, 2020 and 2021
+    dtrack.data.refYears=[2015,2016,2017,2018,2019]
+    dtrack.data.refYears.forEach(yr=>{ // reference 5 year period
         addTrace(yr)
     })
 
